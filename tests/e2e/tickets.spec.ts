8899f64c21a39a442c3355412c5e12df75506576
lior marzouk <liorm@wix.com>
2023-07-16 16:41:22 +0300

    fix(tickets.spec) added timeout

--- tests/e2e/tickets.spec.ts
+++ tests/e2e/tickets.spec.ts
@@ -26,8 +26,11 @@ test.describe('Tickets Page', () => {
     const numOfTicketsToSelect = '2';
 
     await page.waitForSelector('.flowbite-dropdown-target');
+    await page.waitForTimeout(3000);
 
     await (await page.$$('.flowbite-dropdown-target')).pop()!.click();
+
+    await page.waitForTimeout(3000);
 
     await page.waitForSelector(
       `[data-testid="${testIds.TICKET_DETAILS_PAGE.TICKETS_NUMBER_OPTIONS}"]`
